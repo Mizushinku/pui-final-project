61bdd13149943b3d02c35c0657549ea258f30176
sola <s21543775@gmail.com>
2023-05-25 15:16:20 +0800

    popup image test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import ImagePopup from "./containers/ImagePopup";
 
 function App() {
   return (
     <div>
       <h1>PUI Final Project</h1>
+      <ImagePopup></ImagePopup>
     </div>
   );
 }
---
+++ src/containers/ImagePopup.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import "./ImagePopup.css";
+import herta from "../assets/images/herta.jpg";
+
+function ImagePopup() {
+  const [isImageOpen, setIsImageOpen] = useState(false);
+
+  const handleImageClick = () => {
+    setIsImageOpen(true);
+  };
+
+  const closeImage = () => {
+    setIsImageOpen(false);
+  };
+
+  return (
+    <div>
+      <h1>網頁</h1>
+      <div className="image-container">
+        <img
+          src={herta}
+          alt="Original Image"
+          className="original-image"
+          onClick={handleImageClick}
+        />
+        {isImageOpen && (
+          <div className="popup-overlay" onClick={closeImage}>
+            <div className="popup-container">
+              <img
+                src={herta}
+                alt="Popup Image"
+                className="popup-image"
+                onClick={(e) => {
+                  e.stopPropagation();
+                }}
+              />
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default ImagePopup;
