232eaf74adbc7d084b8979677dbc69e5b0f142d9
sola <s21543775@gmail.com>
2023-05-26 16:08:18 +0800

    update imagepopup

--- src/containers/ImagePopup/ImagePopup.js
+++ src/containers/ImagePopup/ImagePopup.js
@@ -35,6 +35,28 @@ function ImagePopup() {
                 }}
               />
             </div>
+            <div class="angry-grid">
+              <div id="item-0">Title: </div>
+              <div id="item-1">&nbsp;</div>
+              <div id="item-2">Caption: </div>
+              <div id="item-3">&nbsp;</div>
+              <div id="item-4">Prompt:</div>
+              <div id="item-5">&nbsp;</div>
+              <div id="item-6">Negative Prompt:</div>
+              <div id="item-7">&nbsp;</div>
+              <div id="item-8">Model:</div>
+              <div id="item-9">&nbsp;</div>
+              <div id="item-10">VAE:</div>
+              <div id="item-11">&nbsp;</div>
+              <div id="item-12">Seed:</div>
+              <div id="item-13">&nbsp;</div>
+              <div id="item-14">Steps:</div>
+              <div id="item-15">&nbsp;</div>
+              <div id="item-16">Sampler:</div>
+              <div id="item-17">&nbsp;</div>
+              <div id="item-18">CFG Scale:</div>
+              <div id="item-19">&nbsp;</div>
+            </div>
           </div>
         )}
       </div>
