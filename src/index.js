a1f75f873442bda1fcaca43053427e1418d5a5cc
Mizushinku <frankie786214593@gmail.com>
2023-05-29 13:00:35 +0800

    add firebase

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB0VZSs-yGOsEadSCobN-1HVWC2wvc0B5k",
+  authDomain: "pui-final-3bc90.firebaseapp.com",
+  projectId: "pui-final-3bc90",
+  storageBucket: "pui-final-3bc90.appspot.com",
+  messagingSenderId: "409645965181",
+  appId: "1:409645965181:web:409643afc790f5f002f614",
+  measurementId: "G-5FNZMDDR0K",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+// const analytics = getAnalytics(app);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
@@ -15,3 +38,5 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+export default db;
