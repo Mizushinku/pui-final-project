21261f40319c8201fdf57ca9a55965aab67721f1
sola <s21543775@gmail.com>
2023-05-31 11:56:52 +0800

    test upload button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import ImageTrack from "./containers/ImageTrack/ImageTrack";
 import ImagePopup from "./containers/ImagePopup/ImagePopup.js";
+import ImageUploader from "./containers/Upload/Upload";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <h1>PUI Final Project</h1>
       <ImageTrack></ImageTrack>
       <ImagePopup></ImagePopup>
+      <ImageUploader></ImageUploader>
     </div>
   );
 }
